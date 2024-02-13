8b63d4dbb97361d56209822c209e702f3ed0999e
Coen Adler <co41co@gmail.com>
2024-02-12 22:41:34 -0800

    updated connectivity tests

--- tests/test_connectivity.py
+++ tests/test_connectivity.py
@@ -1,5 +1,6 @@
 import os
 import ecoscape_connectivity
+import numpy as np
 
 DATA_PATH="tests/assets"
 
@@ -10,6 +11,18 @@ PERMEABILITY_PATH = os.path.join(DATA_PATH, "terrain_permeability.csv")
 CONNECTIVITY_PATH = os.path.join(DATA_PATH, "Outputs/connectivity.tif")
 FLOW_PATH = os.path.join(DATA_PATH, "Outputs/flow.tif")
 
+def gap_calc(r, p):
+    # returns a random sample from the negative binomial probability distribution
+    def f():
+        return np.random.negative_binomial(r, p) + 1
+    return f
+
+def num_gaps_calc(n):
+    # returns a random sample from the normal probability distribution
+    def f():
+            return np.random.normal(n)
+    return f
+
 def test_connectivity():
     ecoscape_connectivity.compute_connectivity(
         habitat_fn=HABITAT_PATH,
@@ -17,5 +30,20 @@ def test_connectivity():
         permeability_dict=PERMEABILITY_PATH,
         connectivity_fn=CONNECTIVITY_PATH,
         flow_fn=FLOW_PATH,
-        num_simulations=20
+        num_simulations=20,
+        gap_crossing=2,
+        num_gaps=10,
     )
+
+    # ecoscape_connectivity.compute_connectivity(
+    #     habitat_fn=HABITAT_PATH,
+    #     terrain_fn=TERRAIN_PATH,
+    #     permeability_dict=PERMEABILITY_PATH,
+    #     connectivity_fn=CONNECTIVITY_PATH,
+    #     flow_fn=FLOW_PATH,
+    #     num_simulations=20,
+    #     gap_crossing=gap_calc(4, 0.8),
+    #     num_gaps=num_gaps_calc(10),
+    # )
+
+test_connectivity()
